af737d11b8546534b5c24c6d9c6677093637b6a0
lukys1234 <24097lp@hvhs.school.nz>
2026-06-08 09:55:26 +1200

    jacob

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello world!")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+let userOrder = ["L&P", "Mince and Cheese Pie", "Fish and Chips", "Pavlova"];
+function getFormInput() {
+
+    
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+    const ORDER_FIELD = document.getElementById("orderField");
+    let order = Number(ORDER_FIELD.value);
+  
+    OUTPUT.innerHTML = "<p> Your name is: "+userName+" </p1>";
+
+  OUTPUT.innerHTML += "You ordered: "+ userOrder[order-1];
+  }
+  

99b7041ddc734c70ad5d90df68e6cba972af40da
lukys1234 <24097lp@hvhs.school.nz>
2026-06-18 15:16:02 +1200

    sadasdadadada

--- script.js
+++ script.js
@@ -15,19 +15,30 @@ function getFormInput() {
   const NAME_FIELD = document.getElementById("nameField");
   const ORDER_FIELD = document.getElementById("orderField");
   const MONEY_FIELD = document.getElementById("moneyField");
+
+  const LANDP_FIELD = document.getElementById("landpField");
+  const MINCEANDCHEESE_FIELD = document.getElementById("minceandcheeseField");
+  const FISHCHIPS_FIELD = document.getElementById("fishandchipsField");
+  const PAVLOVA_FIELD = document.getElementById("pavlovaField");
+
   let userName = NAME_FIELD.value;
-  let order = Number(ORDER_FIELD.value);
-  let cost = Number(ORDER_FIELD.value);
+  let landpAmount  = Number(LANDP_FIELD.value);
+  let mincecheeseAmount = Number(MINCEANDCHEESE_FIELD.value);
+  let fishchipsAmount = Number(FISHCHIPS_FIELD.value);
+  let pavlovaAmount = Number(PAVLOVA_FIELD.value);
+
+let cost =landpAmount *price[0] + mincecheeseAmount * price [1] + fishchipsAmount * price[2] + pavlovaAmount * price[3]
+
   let userMoney = Number(MONEY_FIELD.value);
  
   OUTPUT.innerHTML = "<p> Your name is: " + userName + "";
   OUTPUT.innerHTML += "<p> You have: $" + userMoney;
-  OUTPUT.innerHTML += "You ordered: " + userOrder[order - 1]+ " </p1>";
+  OUTPUT.innerHTML += "You ordered: " +  " </p1>";
 
-  if (userMoney >= price [cost  -1]) {
-    OUTPUT.innerHTML += "<p> You have $"+calculateChange(userMoney, price [cost-1])+ " change"
+  if (userMoney >= cost ) {
+    OUTPUT.innerHTML += "<p> You have $"+calculateChange(userMoney, cost )+ " change"
   } else {
-    OUTPUT.innerHTML += "<p> You can't afford this item, sorry."+ userOrder[order-1] + "</p1>";
+    OUTPUT.innerHTML += "<p> You can't afford this item, sorry."+  + "</p1>";
   }
 
 }

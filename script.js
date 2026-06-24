ce1077a73781d7ee50f089dfe3ca35fef16da7d3
lukys1234 <24097lp@hvhs.school.nz>
2026-06-24 13:29:50 +1200

    asdasdad

--- script.js
+++ script.js
@@ -9,11 +9,52 @@ let price = [5, 7, 12, 15];
 
 for (let i = 0; i < userOrder.length; i++) {
   console.log("menu" + i + ": " + userOrder[i] + price[i] + "<br>")
-  MENU.innerHTML+=userOrder[i] +" $" + price[i] + "<br>"
+  MENU.innerHTML += userOrder[i] + " $" + price[i] + "<br>"
 }
 function calculateChange(_money, _price) {
   let change = _money - _price;
   return change;
+}
+function viewShoppingCart() {
+
+  const LANDP_FIELD = document.getElementById("landpField");
+  const MINCEANDCHEESE_FIELD = document.getElementById("minceandcheeseField");
+  const FISHCHIPS_FIELD = document.getElementById("fishandchipsField");
+  const PAVLOVA_FIELD = document.getElementById("pavlovaField");
+
+  let landpAmount = Number(LANDP_FIELD.value);
+  let mincecheeseAmount = Number(MINCEANDCHEESE_FIELD.value);
+  let fishchipsAmount = Number(FISHCHIPS_FIELD.value);
+  let pavlovaAmount = Number(PAVLOVA_FIELD.value);
+  OUTPUT.innerHTML = "L&Ps: " 
+
+  let cost = landpAmount * price[0] + mincecheeseAmount * price[1] + fishchipsAmount * price[2] + pavlovaAmount * price[3]
+
+  if (landpAmount > 0) {
+    OUTPUT.innerHTML += "L&Ps: " + landpAmount + "= $" + landpAmount * price[0] + "<br>";
+
+  }
+  if (mincecheeseAmount > 0) {
+    OUTPUT.innerHTML += "Mince and cheese Pies: " + mincecheeseAmount + "= $" + mincecheeseAmount * price[1] + "<br>";
+  }
+
+  if (fishchipsAmount > 0) {
+    OUTPUT.innerHTML += "Fish and chips: " + fishchipsAmount + "= $" + fishchipsAmount * price[2] + "<br>";
+
+  }
+
+  if (pavlovaAmount > 0) {
+    OUTPUT.innerHTML += "Pavlova's: " + pavlovaAmount + "= $" + pavlovaAmount * price[3] + "<br>";
+
+  }
+  OUTPUT.innerHTML += "Total cost: $" + cost + "<br>";
+
+
+
+
+
+
+
 }
 
 
@@ -36,31 +77,41 @@ function getFormInput() {
   let cost = landpAmount * price[0] + mincecheeseAmount * price[1] + fishchipsAmount * price[2] + pavlovaAmount * price[3]
 
   if (landpAmount > 0) {
-    OUTPUT.innerHTML += "L&Ps: " + landpAmount + "= $"+ landpAmount * price[0] + "<br>";
+    OUTPUT.innerHTML = "L&Ps: " + landpAmount + "= $" + landpAmount * price[0] + "<br>";
 
   }
   if (mincecheeseAmount > 0) {
-    OUTPUT.innerHTML += "Mince and cheese Pies: " + mincecheeseAmount + "= $"+ mincecheeseAmount * price[1] + "<br>";
+    OUTPUT.innerHTML += "Mince and cheese Pies: " + mincecheeseAmount + "= $" + mincecheeseAmount * price[1] + "<br>";
+  }
 
-  }if (fishchipsAmount > 0) {
-    OUTPUT.innerHTML += "Fish and chips: " + fishchipsAmount + "= $"+ fishchipsAmount * price[2] + "<br>";
+  if (fishchipsAmount > 0) {
+    OUTPUT.innerHTML += "Fish and chips: " + fishchipsAmount + "= $" + fishchipsAmount * price[2] + "<br>";
 
-  }if (pavlovaAmount > 0) {
-    OUTPUT.innerHTML += "pavlova: " + pavlovaAmount + "= $"+ pavlovaAmount * price[3] + "<br>";
+  }
+
+  if (pavlovaAmount > 0) {
+    OUTPUT.innerHTML += "pavlova: " + pavlovaAmount + "= $" + pavlovaAmount * price[3] + "<br>";
 
   }
 
   let userMoney = Number(MONEY_FIELD.value);
 
 
+  OUTPUT.innerHTML += "Total cost: $" + cost + "<br>";
 
-  OUTPUT.innerHTML = "<p> Your name is: " + userName + "<br>";
+  OUTPUT.innerHTML += "<p> Your name is: " + userName + "<br>";
   OUTPUT.innerHTML += "<p> You have: $" + userMoney;
 
   if (userMoney >= cost) {
     OUTPUT.innerHTML += "<p> You have $" + calculateChange(userMoney, cost) + " change"
   } else {
-    OUTPUT.innerHTML += "<p> You can't afford this item, sorry." + + "</p1>";
+    OUTPUT.innerHTML += "<p> You can't afford this item, sorry.</p1>";
   }
 
+  if (cost > userMoney) {
+    OUTPUT.innerHTML += " Your items were $" + cost + " ,You have $ " + userMoney + " you cant afford these items, sorry."
+  } else {
+    OUTPUT.innerHTML += "Thanks for shopping at my Cafe , have a good day."
+
+  }
 }
